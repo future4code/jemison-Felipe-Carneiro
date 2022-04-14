8904b130d91ab12d4854acfbb96465ae91d95de6
Felipe Carneiro <ffsrcarneiro@gmail.com>
2022-04-14 19:42:16 -0300

    variaveis

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 // imprimindo no console
 console.log("Olá mundo!")
@@ -40,3 +41,89 @@ const idadeEmNumeros = 23;
 const idadeEmStringConversao = idadeEmNumeros.toString();
 console.log("Em numero: ", idadeEmNumeros);
 console.log("Em string:", idadeEmStringConversao);
+=======
+// Exercícios de interpretação de código
+
+// Exercício 1 : Vai aparecer no console ( 10 e 5 )
+// Exercício 2 : vai aparecer no console ( 10 , 10 e 10 )
+// Exercício 3 : let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+// Exercício 3 : let ganhosDiarios = prompt(" Quanto você recebe por dia?")
+
+//Exercícios de escrita de código
+
+// Exercício de Escrita 1: Construa um programa, seguindo os seguintes passos:
+
+let meuNome = prompt("Qual seu nome ?");
+let minhaIdade = prompt("Qual sua idade?");
+console.log(typeof minhaIdade)
+console.log(typeof meuNome)
+
+let nome = ("Felipe Carneiro");
+let idade = ("35");
+
+typeof nome
+typeof idade
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+console.log("Olá",nome,"Você tem",idade,"anos");
+
+// resposta1 typeof: apareceu undefined impresso devido nenhuma variavel conter um valor atribuido.
+// resposta2 typeof: ao imprimir nome de usuário e idade apareceu string nos 2. mesmo que seja numero vai aparecer que é uma string. so mudaria se colocar NUMBER na frente.
+
+
+// Exercício de Escrita 2: Escreva um programa que faça 3 perguntas de Sim ou Não, armazenado em uma variável.
+
+let perguntaUm = "você é Flamengo?"
+let perguntaDois = "você tem 3 cachorros?"
+let perguntaTres = "seu carro é um siena?"
+let respostaUm = "sim"
+let respostaDois = "não"
+let respostaTres = "sim"
+
+console.log(perguntaUm,"-",respostaUm)
+console.log(perguntaDois,"-",respostaDois)
+console.log(perguntaTres,"-",respostaTres)
+
+// Exercício de Escrita 3: Crie a lógica que deve ser inserida no código para que os valores de a e b sejam trocados, independente de qual valor essas variáveis assumam inicialmente (ou seja: não basta dizer que a = 25 e b = 10 porque se os valores iniciais mudarem, a lógica do seu programa teria que mudar também). 
+
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+
+// Aqui faremos uma lógica para trocar os valores
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+
+// DESAFIOS 
+
+// Faça um programa que receba dois números do usuário e faça as seguintes operações, imprimindo os resultados no console da seguinte forma:
+
+let numeroUm = Number(prompt("Digite um numero"))
+let numeroDois = Number(prompt("Digite um numero "))
+
+let soma = numeroUm + numeroDois
+let multi = numeroUm * numeroDois
+
+console.log(soma)
+console.log(multi)
+
+console.log("O primeiro número somado ao segundo número resulta em:",soma)
+console.log("O primeiro número multiplicado pelo segundo número resulta em:",multi)
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> 164a0c98283c961b390cbb37e832e41acdcc7b79
